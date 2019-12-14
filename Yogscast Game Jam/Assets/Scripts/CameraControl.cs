27e61c248bb345be52f6759b5bee7c63bcd9dbd9
Ryan <Hoodrn03@gmail.com>
2019-12-14 10:23:41 +0000

    Movement added
    I have creaed a basic movement script which will allow for the player to move a cube around the game.
    
    I have also added a camera control script which will allow for the camera to follow the player.

---
+++ Yogscast Game Jam/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    [SerializeField]
+    GameObject m_CameraTarget; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position != m_CameraTarget.transform.position)
+        {
+            Vector3 l_NewPos;
+
+            l_NewPos.x = m_CameraTarget.transform.position.x;
+            l_NewPos.y = m_CameraTarget.transform.position.y;
+            l_NewPos.z = transform.position.z; 
+
+            transform.position = l_NewPos;
+        }
+    }
+}
---
+++ Yogscast Game Jam/Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    float m_fSpeed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float l_fYTranslation = (Input.GetAxis("Vertical") * m_fSpeed) * Time.deltaTime;
+
+        float l_fXTranslation = (Input.GetAxis("Horizontal") * m_fSpeed) * Time.deltaTime;
+
+        transform.Translate(l_fXTranslation, l_fYTranslation, 0);
+    }
+}
